7cee0e06bbeaa1626d9df4f9f80b95af1c0e7663
Aashish_Rimal <aashishrimal2003@gmail.com>
2025-01-12 17:54:10 +0545

    submit google

---
+++ google.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google</title>
+</head>
+<body style="background-color:black;">
+    
+    <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google Logo" style="width:150px;margin-left: 650px;margin-top: 50px;">
+    <form action="https://www.google.com/search" method="get" style="margin-top: 20px;margin-left: 545px;">
+        <input type="text" name="q" placeholder="Search Google..." style="padding: 10px; width: 300px;">
+        <button type="submit" style="padding: 10px 20px;">Search</button>
+    </form>
+</body>
+</html>
