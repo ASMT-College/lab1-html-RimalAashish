751f2b9336ff4fc1930db0914c8868680d66e856
Aashish_Rimal <aashishrimal2003@gmail.com>
2025-01-25 19:43:53 +0545

    Table design

---
+++ Practice/script.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Script</title>
+</head>
+<body>
+    X<sub>1</sub><sup>2</sup> + X<sub>2</sub><sup>2</sup> = Y
+</body>
+</html>
